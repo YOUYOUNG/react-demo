90ceebbc53d09996bad36b69bb248704d4d07f83
YOUOYUNG <360111767@qq.com>
2021-10-09 09:14:38 +0800

    【first】createPortal

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import DialogPage from './pages/DialogPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DialogPage/>
+    </div>
+  );
+}
+
+export default App;
